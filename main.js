83439403003f2d958a79f653f5f4ddea5757735d
NOTIAMRAZER <77327520+NOTIAMRAZER@users.noreply.github.com>
2022-01-07 16:54:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 video = "";
 status = "";
 input_value = "";
+objects = [];
 
-function preload(){
-    video = createVideo('video.mp4');
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(480, 380);
     video.hide();
 }
 
-function setup(){
-    canvas = createCanvas(480, 370);
-    canvas.center();
+function draw(){
+    image(video, 0, 0, 480, 380);
+
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input_value){
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = input_value + " found!";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(input_value + " found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("object_status").innerHTML = input_value + " not found!";
+            }
+        }
+    }
 }
 
-function draw(){
-    image(video, 0, 0, 480, 370);
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start(){
@@ -26,7 +60,4 @@ function start(){
 function modelLoaded(){
     console.log("Model Loaded!");
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
 }
